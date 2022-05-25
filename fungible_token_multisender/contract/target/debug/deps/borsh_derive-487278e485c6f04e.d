ad52299326203e97519221ab1c806b04ca583f53
YellingOilbird <@guacharo.w3@yahoo.com>
2022-05-25 16:03:41 +0400

    commit

---
+++ fungible_token_multisender/contract/target/debug/deps/borsh_derive-487278e485c6f04e.d
@@ -0,0 +1,5 @@
+/Users/macbookpro/Documents/mmmm/multisender_ft/fungible_token_multisender/contract/target/debug/deps/libborsh_derive-487278e485c6f04e.dylib: /Users/macbookpro/.cargo/registry/src/github.com-1ecc6299db9ec823/borsh-derive-0.9.3/src/lib.rs
+
+/Users/macbookpro/Documents/mmmm/multisender_ft/fungible_token_multisender/contract/target/debug/deps/borsh_derive-487278e485c6f04e.d: /Users/macbookpro/.cargo/registry/src/github.com-1ecc6299db9ec823/borsh-derive-0.9.3/src/lib.rs
+
+/Users/macbookpro/.cargo/registry/src/github.com-1ecc6299db9ec823/borsh-derive-0.9.3/src/lib.rs:
